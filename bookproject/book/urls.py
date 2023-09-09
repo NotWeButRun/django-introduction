cebbc8b859e348b1dbbe2903f6c1b5314c326958
たかふみ ほりえ <onion.dedede@gmail.com>
2023-09-09 05:23:48 +0000

    [feature] View の削除/追加を実装

--- bookproject/book/urls.py
+++ bookproject/book/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListBookView, DetailBookView
+from .views import ListBookView, DetailBookView, CreateBookView, DeleteBookView, UpdateBookView
 
 urlpatterns = [
-    path("book/", ListBookView.as_view()),
-    path("book/<int:pk>/detail/", DetailBookView.as_view()),
+    path("book/", ListBookView.as_view(), name="list-book"),
+    path("book/<int:pk>/detail/", DetailBookView.as_view(), name="detail-book"),
+    path("book/create/", CreateBookView.as_view(), name="create-book"),
+    path("book/<int:pk>/update/", UpdateBookView.as_view(), name="update-book"),
+    path("book/<int:pk>/delete/", DeleteBookView.as_view(), name="delete-book"),
 ]
--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, DeleteView, UpdateView
+)
 from .models import Book
 
 
@@ -11,3 +14,20 @@ class ListBookView(ListView):
 class DetailBookView(DetailView):
     model = Book
     template_name = "book/book_detail.html"
+
+class CreateBookView(CreateView):
+    model = Book
+    template_name = "book/book_create.html"
+    fields = ("title", "price", "category")
+    success_url = reverse_lazy("list-book")
+
+class DeleteBookView(DeleteView):
+    model = Book
+    template_name = "book/book_confirm_delete.html"
+    success_url = reverse_lazy("list-book")
+
+class UpdateBookView(UpdateView):
+    model = Book
+    template_name = "book/book_update.html"
+    fields = ("title", "price", "category")
+    success_url = reverse_lazy("list-book")
